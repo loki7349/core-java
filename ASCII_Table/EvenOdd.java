cdbaaa2de592243e1f3709e9603f8b5aab5399f0
loki7349 <lokeshargon19l@gmail.com>
2024-01-25 23:19:33 +0530

    core java Q

---
+++ ASCII_Table/EvenOdd.java
@@ -0,0 +1,13 @@
+package ASCII_Table;
+
+public class EvenOdd {
+    public static void main(String[] args){
+        int num = 29;
+
+        if(num % 2 == 0){
+            System.out.println(num + " is Even");
+        }else{
+            System.out.println(num + " is Odd");
+        }
+    }
+}
---
+++ ASCII_Table/PosOrNeg.java
@@ -0,0 +1,16 @@
+package ASCII_Table; 
+
+public class PosOrNeg{
+    public static void main(String[] args){
+        int num=5;
+
+        if(num > 0){
+            System.out.println("The number is positive");
+        }else if(num<0){
+            System.out.println("The number is Negative");
+        }else{
+            System.out.println("Zero");
+        }
+
+    }
+}
